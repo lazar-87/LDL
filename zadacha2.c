5fba253be4a2ceb90fda40577a93ef535c7b59d3
lazar-87 <118368236+lazar-87@users.noreply.github.com>
2022-11-21 09:46:44 +0200

    Add files via upload

---
+++ zadacha2.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a; int b; 
+   printf("въведете дължината на правоъгълника");
+   scanf("%d",&a);
+   printf("въведете ширината на правоъгълника");
+   scanf("%d",&b);
+    printf("лицето на правоъгълника е %d", a*b);
+
+    return 0;
+}
