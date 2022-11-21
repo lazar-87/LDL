99bdd920e11e4a02e10f4f9cf92cb01e52540717
lazar-87 <118368236+lazar-87@users.noreply.github.com>
2022-11-21 09:46:55 +0200

    Add files via upload

---
+++ zadacha3.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a; int b; int h;
+   printf("въведете дължината на трапец a ");
+   scanf("%d",&a);
+   printf("въведете дължината на трапец b ");
+   scanf("%d",&b);
+   printf("въведете височината на трапец h ");
+   scanf("%d",&h);
+    printf("лицето на трапеца е %d", (a+b)*h/2);
+
+    return 0;
+}
